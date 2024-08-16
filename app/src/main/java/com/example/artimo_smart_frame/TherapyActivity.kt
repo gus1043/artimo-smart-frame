8f58623597664d19efcf0ee6b388acd6c7b4aae0
JihyeonChoi <gus1043@gmail.com>
2024-08-16 18:48:03 +0900

    feat : 테라피 페이지 레이아웃 생성

---
+++ app/src/main/java/com/example/artimo_smart_frame/TherapyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.artimo_smart_frame
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class TherapyActivity: FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theraphy) // 레이아웃 설정
+    }
+}
