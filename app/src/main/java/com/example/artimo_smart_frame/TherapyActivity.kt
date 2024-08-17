4599e2c1e5c744ea98719473cee95683c59d260c
JihyeonChoi <gus1043@gmail.com>
2024-08-18 00:54:05 +0900

    feat : 웹뷰를 통한 영상 재생 기능 구현

--- app/src/main/java/com/example/artimo_smart_frame/TherapyActivity.kt
+++ app/src/main/java/com/example/artimo_smart_frame/TherapyActivity.kt
@@ -1,21 +1,17 @@
 package com.example.artimo_smart_frame
 
 import android.content.Intent
-import android.util.Log
-import android.widget.MediaController
-import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.VideoView
 import androidx.fragment.app.FragmentActivity
-import java.io.IOException
 
 class TherapyActivity : FragmentActivity() {
     private lateinit var gallarybtn: Button
-    private lateinit var therapyArt: VideoView
+    private lateinit var therapyArt: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +19,9 @@ class TherapyActivity : FragmentActivity() {
 
         therapyArt = findViewById(R.id.therapyart)
         gallarybtn = findViewById(R.id.gallarybtn)
+        val videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        therapyArt.loadUrl(videoURL)
+
 
         gallarybtn.setOnFocusChangeListener { v, hasFocus ->
             if (hasFocus) {
@@ -37,26 +36,21 @@ class TherapyActivity : FragmentActivity() {
             startActivity(intent)
         }
     }
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
-            // 버튼의 가시성을 토글
+
+    override fun onBackPressed() {
+        // 갤러리 버튼이 보이면 뒤로 가기
+        if (gallarybtn.visibility == View.VISIBLE) {
+            super.onBackPressed() // 기본 뒤로 가기 동작 수행
+        } else {
+            // 갤러리 버튼이 보이지 않으면 보이게 하기
             gallarybtn.visibility = View.VISIBLE
+            // 웹뷰의 포커스를 제거
+            therapyArt.clearFocus()
+            // 웹뷰의 포커스를 제거
+            therapyArt.clearFocus()
 
             // 버튼에 포커스 설정
             gallarybtn.requestFocus()
-
-            return true // 이벤트가 처리됨
-        }
-        return super.onKeyDown(keyCode, event)
-    }
-
-    override fun onBackPressed() {
-        // 갤러리 버튼이 보이지 않을 때만 기본 뒤로 가기 동작 수행
-        if (gallarybtn.visibility == View.GONE) {
-            super.onBackPressed() // 기본 뒤로 가기 동작 수행
-        } else {
-            // 갤러리 버튼이 보이는 경우 버튼 숨기기
-            gallarybtn.visibility = View.GONE
         }
     }
 }
