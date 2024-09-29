1bf0d92d6fd83bc8a17b0276114332c71d7c9b76
JihyeonChoi <gus1043@gmail.com>
2024-09-29 20:45:48 +0900

    feat : BaseURI 숨기기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
+
+// properties 객체를 생성하고 로드
+val properties = Properties()
+val propertiesFile = project.rootProject.file("local.properties") // 속성 파일 경로
+properties.load(propertiesFile.inputStream())
 
 android {
     namespace = "com.example.artimo_smart_frame"
@@ -14,6 +21,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        // Base_URL을 BuildConfig에 추가
+        val baseUrl = properties["BASE_URL"]?.toString() ?: "https://default-url.com/"
+        buildConfigField("String", "BASE_URL", baseUrl)
     }
 
     buildTypes {
@@ -44,4 +54,8 @@ dependencies {
     //defaultartfragment의 명화용
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation ("com.google.code.gson:gson:2.8.9")
+    //server 연결
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
